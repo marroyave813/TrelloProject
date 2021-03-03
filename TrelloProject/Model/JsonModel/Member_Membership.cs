1180331f9dd4736e659425601c0876728425c298
Mauricio Arroyave Panesso <mauricioarroyave@gmail.com>
2021-03-03 18:22:40 -0500

    Memebrs test cases and adjustment to the boards alternate course test cases

---
+++ TrelloProject/Model/JsonModel/Member_Membership.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrelloProject.Model.JsonModel
+{
+	public class Member_Membership
+	{
+		public string id { get; set; }
+		public List<Member> members { get; set; }
+		public List<Membership> memberships { get; set; }
+
+		public static Member_Membership Deserialize(IRestResponse response)
+		{
+			Member_Membership repoReponse = JsonConvert.DeserializeObject<Member_Membership>(response.Content.ToString());
+			return repoReponse;
+		}
+	}
+}
